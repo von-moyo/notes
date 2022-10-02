722b4ea2c5102fcbf8de00db74b7ec4c35def220
von-moyo <florishadekogbe@gmail.com>
2022-10-02 11:38:09 +0100

    Updates

---
+++ precache-manifest.30df6334d4f9da21f1b5236f5da690a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a668dbf4042d0911ba9",
+    "url": "/notes/static/css/main.d6b3adad.chunk.css"
+  },
+  {
+    "revision": "2a668dbf4042d0911ba9",
+    "url": "/notes/static/js/main.2a668dbf.chunk.js"
+  },
+  {
+    "revision": "922505d4d7b2de10a047",
+    "url": "/notes/static/js/1.922505d4.chunk.js"
+  },
+  {
+    "revision": "1291e1b612bc298497c9",
+    "url": "/notes/static/js/runtime~main.1291e1b6.js"
+  },
+  {
+    "revision": "a97322965c816016ce5d82a3236ac5c2",
+    "url": "/notes/index.html"
+  }
+];
